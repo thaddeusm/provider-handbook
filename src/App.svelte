358a562fec88989500c77eae8908538f85eb9e71
Thaddeus McCleary <thaddeusmccleary@icloud.com>
2021-02-12 09:34:11 -0600

    Add global styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,25 @@
 <script>
 	import {Route} from 'tinro'; 
 
-	import Test from './Test.svx';
 </script>
 
-<main>
-	<Test></Test>
-</main>
+<div id="app">
+	<header></header>
+	<main>
+		<h1>Title Text</h1>
+		<h2>Heading Text</h2>
+		<h3>Subheading Text</h3>
+		<p>Body text</p>
+		<a class="action-button" href="https://thaddeus.education" target="_blank">Thaddeus</a>
+		<a class="regular-button" href="https://thaddeus.education" target="_blank">Thaddeus</a>
+		<section class="jumbotron">
+			<h2>a test</h2>
+		</section>
+	</main>
+	<footer>
+		<h6>Caption Text</h6>
+	</footer>
+</div>
 
 <style>
 	
