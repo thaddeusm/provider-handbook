3f3c9e53dbf03e3e954c85ce84be83bdb2b1e159
Thaddeus McCleary <thaddeusmccleary@icloud.com>
2021-02-17 17:13:44 -0600

    Begin mobile search component

--- src/Components/HandbookSearch.svelte
+++ src/Components/HandbookSearch.svelte
@@ -7,11 +7,25 @@
 	const dispatch = createEventDispatcher();
 
 	let query = '';
+
+	function closeSearch() {
+		dispatch('close-search');
+	}
 </script>
 
 <aside>
 	<section id="searchBox">
-		<input type="text" bind:value={query}>
+		<section id="inputArea">
+			<section id="searchIcon">
+				<Search color={'#000000'} width={'2rem'} height={'2rem'} />
+			</section>
+			<input type="text" bind:value={query}>
+		</section>
+		<section id="closeArea">
+			<button on:click={closeSearch}>
+				<Close width={'2rem'} height={'2rem'}  />
+			</button>
+		</section>
 	</section>
 	<section id="results">
 		
@@ -29,16 +43,50 @@
 		z-index: 3000;
 		display: grid;
 		align-items: center;
-		grid-template-rows: 25% 75%;
+		grid-template-rows: 15% 85%;
+	}
+
+	#searchBox {
+		display: grid;
+		grid-template-columns: .1fr 1.4fr .5fr;
+		grid-template-areas: ". inputArea closeArea";
+		align-items: center;
+	}
+
+	#inputArea {
+		border: 1px solid var(--brand);
+		border-radius: 1px;
+		padding: 10px 5px;
+		grid-area: inputArea;
+		display: grid;
+		grid-template-columns: 20% 80%;
+		grid-template-areas: "searchIcon input";
+		align-items: center;
+	}
+
+	#closeArea {
+		grid-area: closeArea;
+		text-align: center;
 	}
 
 	input {
-		border: 1px solid var(--brand);
-		border-radius: 1px;
-		padding: 15px;
 		font-size: 21px;
 		letter-spacing: 2px;
 		text-transform: uppercase;
 		font-family: "Montserrat-Bold";
+		background: none;
+		border: none;
+		outline: none;
+		display: block;
+		max-width: 180px;
+		grid-area: input;
+	}
+
+	input:focus {
+		outline: none;
+	}
+
+	#searchIcon {
+		grid-area: searchIcon;
 	}
 </style>
