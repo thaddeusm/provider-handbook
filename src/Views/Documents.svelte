4e55ecbd8b0d93693e10cc15792af842bb22cf74
Thaddeus McCleary <thaddeusmccleary@icloud.com>
2021-04-01 14:54:25 -0500

    Add placeholders for future pages

--- src/Views/Documents.svelte
+++ src/Views/Documents.svelte
@@ -1,1 +1,16 @@
-<h1>Documents</h1>
+<div class="container">
+	<h2></h2>
+	<p></p>
+	<footer>
+		<h6>sponsored by the U.S. Department of State</h6>
+	</footer>
+</div>
+
+<style>
+.container {
+	height: 100%;
+	display: grid;
+	grid-template-rows: 65% auto 100px;
+	align-items: center;
+}
+</style>
